7214aa51f3dc5173db22d859d0d63da540d7ba96
Edward Snow <eddysnow@googlemail.com>
2018-07-18 19:58:45 +0100

    init

---
+++ deploy-ansible-command.ps1
@@ -0,0 +1,13 @@
+﻿& "$env:userprofile\git\labs\deploy-infra.ps1" `
+    -gitPath "$env:userprofile\git\labs" `
+    -tagValueProduct "lab" `
+    -tagValueContact "eddysnow@googlemail.com" `
+    -awsAccessKey $(get-content C:\temp\snowcoAccessKey.txt -ErrorAction SilentlyContinue) `
+    -awsSecretKey $(get-content C:\temp\snowcoSecretKey.txt -ErrorAction SilentlyContinue) `
+    -region "eu-west-1" `
+    -components vpc `
+    -stackStemName "ansible" `
+    -deploymentBucket "722777194664-eddy-scratch" `
+    -keyName eddy-lab@gmail.com `
+    -confirmWhenStackComplete
+

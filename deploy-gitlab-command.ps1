4ca0e846fb37ca16eb05bf82c3b5c7406d936d31
Eddy Snow <eddysnow@googlemail.com>
2018-07-20 11:37:11 +0100

    parametise aws account deployment variables

--- deploy-gitlab-command.ps1
+++ deploy-gitlab-command.ps1
@@ -1,27 +1,48 @@
-﻿& "$env:userprofile\git\labs\deploy-infra.ps1" `
+﻿$awsAccount = "asds"
+
+if ($awsAccount -eq "personal") {
+
+    $awsAccessKey = $(get-content C:\temp\snowcoAccessKey.txt -ErrorAction SilentlyContinue)
+    $awsSecretKey = $(get-content C:\temp\snowcoSecretKey.txt -ErrorAction SilentlyContinue)
+    $deploymentBucket = "722777194664-eddy-scratch"
+    $tagValueContact = "eddysnow@googlemail.com"
+    $keyName = "eddy-lab@gmail.com"
+
+} else {
+
+    $awsAccessKey = $(get-content C:\temp\awsAccessKey.txt -ErrorAction SilentlyContinue)
+    $awsSecretKey = $(get-content C:\temp\awsSecretKey.txt -ErrorAction SilentlyContinue)
+    $deploymentBucket = "357128852511-eddy-scratch"
+    $tagValueContact = "eddy.snow@intapp.com"
+    $keyName = "eddy-scratch@intapp.com"
+
+}
+
+
+& "$env:userprofile\git\labs\deploy-infra.ps1" `
     -gitPath "$env:userprofile\git\labs" `
     -tagValueProduct "lab" `
-    -tagValueContact "eddysnow@googlemail.com" `
-    -awsAccessKey $(get-content C:\temp\snowcoAccessKey.txt -ErrorAction SilentlyContinue) `
-    -awsSecretKey $(get-content C:\temp\snowcoSecretKey.txt -ErrorAction SilentlyContinue) `
+    -tagValueContact $tagValueContact `
+    -awsAccessKey $awsAccessKey `
+    -awsSecretKey $awsSecretKey `
     -region "eu-west-1" `
     -components vpc `
     -stackStemName "gitlab" `
-    -deploymentBucket "722777194664-eddy-scratch" `
-    -keyName eddy-lab@gmail.com `
+    -deploymentBucket $deploymentBucket `
+    -keyName $keyName `
     -confirmWhenStackComplete
 
 & "$env:userprofile\git\labs\deploy-infra.ps1" `
 	-gitPath "$env:userprofile\git\labs" `
 	-tagValueProduct "lab" `
-	-tagValueContact "eddysnow@googlemail.com" `
-	-awsAccessKey $(get-content C:\temp\snowcoAccessKey.txt -ErrorAction SilentlyContinue) `
-	-awsSecretKey $(get-content C:\temp\snowcoSecretKey.txt -ErrorAction SilentlyContinue) `
+	-tagValueContact $tagValueContact `
+	-awsAccessKey $awsAccessKey `
+	-awsSecretKey $awsSecretKey `
 	-region "eu-west-1" `
 	-components postgresrds `
 	-stackStemName "gitlab" `
-	-deploymentBucket "722777194664-eddy-scratch" `
-	-keyName eddy-lab@gmail.com `
+	-deploymentBucket $deploymentBucket `
+	-keyName $keyName `
     -confirmWhenStackComplete `
     -dbSuffix gitlabDb `
     -dbInstanceClass db.t2.small `
@@ -31,14 +52,14 @@
 & "$env:userprofile\git\labs\deploy-infra.ps1" `
 	-gitPath "$env:userprofile\git\labs" `
 	-tagValueProduct "lab" `
-	-tagValueContact "eddysnow@googlemail.com" `
-	-awsAccessKey $(get-content C:\temp\snowcoAccessKey.txt -ErrorAction SilentlyContinue) `
-	-awsSecretKey $(get-content C:\temp\snowcoSecretKey.txt -ErrorAction SilentlyContinue) `
+	-tagValueContact $tagValueContact `
+	-awsAccessKey $awsAccessKey `
+	-awsSecretKey $awsSecretKey `
 	-region "eu-west-1" `
 	-components gitlablinuxEc2Asg `
 	-stackStemName "gitlab" `
-	-deploymentBucket "722777194664-eddy-scratch" `
-	-keyName eddy-lab@gmail.com `
+	-deploymentBucket $deploymentBucket `
+	-keyName $keyName `
     -ec2AsgInstanceType t2.small `
     -ec2AsgMultiAz False `
     -ec2AsgImage amzn-ami-hvm-*-x86_64-gp2* `

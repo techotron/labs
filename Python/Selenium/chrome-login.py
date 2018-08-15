06f10891a63a350db0a811cd7fb0b9a8f972ec61
Eddy Snow <eddysnow@googlemail.com>
2018-08-15 09:22:39 +0100

    chrome example login to site

--- Python/Selenium/chrome-login.py
+++ Python/Selenium/chrome-login.py
@@ -5,21 +5,7 @@ import time
 from selenium import webdriver
 
 browser = webdriver.Chrome('C:\Users\eddys\Documents\Programs\chromedriver.exe')
-# browser.get("https://team-avengers.timedev.intapp.com")
-#
-# try:
-#     reportBtn = browser.find_element_by_name('Reports')
-#     print('Found <%s> element with that class name!' % (reportBtn.tag_name))
-# except:
-#     print('Was not able to find an element with that name.')
-
-
 browser.get('https://team-avengers.timedev.intapp.com/Reports')
-
-# emailTextBox = browser.find_element_by_id('email')
-# emailTextBox.click()
-# emailTextBox.send_keys('peterw')
-
 browser.find_element_by_id('kc-submit').click()
 browser.find_element_by_id('username').send_keys('MYUSERNAME')
 browser.find_element_by_id('password').send_keys('MYPASSWORD')

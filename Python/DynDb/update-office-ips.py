2275c31a7c81853925f67159c9a20cd7f1437c57
Eddy Snow <eddysnow@googlemail.com>
2018-10-12 17:26:47 +0100

    fix computer name to ip checker

--- Python/DynDb/update-office-ips.py
+++ Python/DynDb/update-office-ips.py
@@ -2,6 +2,7 @@ import boto3
 import json
 import sys
 import socket
+from dns import resolver
 
 accessKey = sys.argv[1]
 secretKey = sys.argv[2]
@@ -14,12 +15,25 @@ mysession = boto3.session.Session(aws_access_key_id=accessKey, aws_secret_access
 
 dynamodb = mysession.resource('dynamodb', region_name=region)
 table = dynamodb.Table(tableName)
+resolver = resolver.Resolver()
+resolver.nameservers = ['10.146.1.8']
 
 result = table.scan()
 jsonOutput = json.dumps(result)
 
 for computer in computerNames:
-    ip = socket.gethostbyname(computer)
+    try:
+        ip = socket.gethostbyname(computer)
+    except:
+        answers = resolver.query(computer)
+        for rdata in answers:
+            ip = (rdata.address)
+
+    if computer == 'eddys-3620':
+        answers = resolver.query(computer)
+        for rdata in answers:
+            ip = (rdata.address)
+
     table.put_item(Item={
         'ComputerName': '%s' % computer,
         'IP': '%s' % ip

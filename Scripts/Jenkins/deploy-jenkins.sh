0ae5376c1c044413bf5ae7d53119ed2483be2f22
Eddy Snow <eddysnow@gmail.com>
2018-12-31 14:05:34 +0000

    add jenkins build agent asg to deployment script

--- Scripts/Jenkins/deploy-jenkins.sh
+++ Scripts/Jenkins/deploy-jenkins.sh
@@ -8,6 +8,7 @@ echo "[$(date)] - Uploading templates to s3"
 aws s3api put-object --bucket 278942993584-eddy-scratch --key git/eddy-jenkins/vpc.yml --region $AWS_REGION --profile $AWS_PROFILE --body /Users/eddys/git/labs/Cloudformation/vpc.yml
 aws s3api put-object --bucket 278942993584-eddy-scratch --key git/eddy-jenkins/efs-volume.yml --region $AWS_REGION --profile $AWS_PROFILE --body /Users/eddys/git/labs/Cloudformation/efs-volume.yml
 aws s3api put-object --bucket 278942993584-eddy-scratch --key git/eddy-jenkins/ec2-asg-docker-jenkins-linux.yml --region $AWS_REGION --profile $AWS_PROFILE --body /Users/eddys/git/labs/Cloudformation/ec2-asg-docker-jenkins-linux.yml
+aws s3api put-object --bucket 278942993584-eddy-scratch --key git/eddy-jenkins/ec2-asg-jenkins-agent-centos.yml --region $AWS_REGION --profile $AWS_PROFILE --body /Users/eddys/git/labs/Cloudformation/ec2-asg-jenkins-agent-centos.yml
 
 echo "[$(date)] - vpc stack"
 if [ ! $(aws cloudformation describe-stacks --region $AWS_REGION --profile $AWS_PROFILE | jq '.Stacks[].StackName' | grep eddy-vpc) ]; then
@@ -34,4 +35,13 @@ if [ ! $(aws cloudformation describe-stacks --region $AWS_REGION --profile $AWS_
 else
     echo "[$(date)] - Updating eddy-jenkins stack"
     aws cloudformation update-stack --stack-name eddy-jenkins --template-url https://s3-eu-west-1.amazonaws.com/278942993584-eddy-scratch/git/eddy-jenkins/ec2-asg-docker-jenkins-linux.yml --profile $AWS_PROFILE --region $AWS_REGION --capabilities CAPABILITY_IAM;
+fi
+
+echo "[$(date)] - jenkins centos agent asg stack"
+if [ ! $(aws cloudformation describe-stacks --region $AWS_REGION --profile $AWS_PROFILE | jq '.Stacks[].StackName' | grep eddy-jenkins-agents-centos) ]; then
+    echo "[$(date)] - Creating eddy-jenkins-agents-centos stack"
+    aws cloudformation create-stack --stack-name eddy-jenkins-agent-centos --template-url https://s3-eu-west-1.amazonaws.com/278942993584-eddy-scratch/git/eddy-jenkins/ec2-asg-jenkins-agent-centos.yml --profile $AWS_PROFILE --region $AWS_REGION --capabilities CAPABILITY_IAM;
+else
+    echo "[$(date)] - Updating eddy-jenkins stack"
+    aws cloudformation update-stack --stack-name eddy-jenkins-agent-centos --template-url https://s3-eu-west-1.amazonaws.com/278942993584-eddy-scratch/git/eddy-jenkins/ec2-asg-jenkins-agent-centos.yml --profile $AWS_PROFILE --region $AWS_REGION --capabilities CAPABILITY_IAM;
 fi

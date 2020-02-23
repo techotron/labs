04fc56dc14801d62396367554742c08baddfece1
techotron <eddysnow@googlemail.com>
2020-02-23 23:23:53 +0000

    add resources

--- Terraform/Linux Academy - AWS with Ansible and Terraform/variables.tf
+++ Terraform/Linux Academy - AWS with Ansible and Terraform/variables.tf
@@ -9,3 +9,22 @@ variable "cidrs" {
 
 variable "localip" {}
 variable "domain_name" {}
+
+variable "db_instance_class" {}
+variable "dbname" {}
+variable "dbuser" {}
+variable "dbpassword" {}
+variable "dev_instance_type" {}
+variable "dev_ami" {}
+variable "key_name" {}
+variable "public_key_path" {}
+variable "elb_healthy_threshold" {}
+variable "elb_unhealthy_threshold" {}
+variable "elb_timeout" {}
+variable "elb_interval" {}
+variable "lc_instance_type" {}
+variable "asg_max" {}
+variable "asg_min" {}
+variable "asg_grace" {}
+variable "asg_hct" {}
+variable "asg_cap" {}
